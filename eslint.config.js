a3cea7fc303f39b4b7a390d4b4bc6996254dbd74
John Kalumba <179124012+JohnkayFundz@users.noreply.github.com>
2026-09-24 13:57:29 +0100

    Add strict ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import reactHooks from 'eslint-plugin-react-hooks'
+
+export default tseslint.config(
+  { ignores: ['dist/**', 'node_modules/**', '.vercel/**'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['src/**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.es2022 },
+    },
+    plugins: { 'react-hooks': reactHooks },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
+)
